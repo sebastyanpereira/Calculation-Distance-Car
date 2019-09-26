035142070bac0884ae27ea5a081f55802dd22bc5
dmitry.novikov <student@ya.ru>
2019-09-26 13:13:30 +0300

    fixes #2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner consumption = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter your car fuel consumption, expressed in litres per 100 km: ");
-        double fuelConsumption = consumption.nextDouble();
-        Scanner amount = new Scanner(System.in);
+        double fuelConsumption = keyboard.nextDouble();
         System.out.println("Please enter amount of fuel in your car");
-        double fuelAmount = consumption.nextDouble();
+        double fuelAmount = keyboard.nextDouble();
         int approximateDistance = (int) (fuelAmount / fuelConsumption);
         System.out.println("Distance which your car can go equals " + approximateDistance + " km");
     }
